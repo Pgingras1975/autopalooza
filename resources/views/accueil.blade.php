3b9fb43f530cc99cc1888562613c8d515ad8a09d
CSTJ\1760981 <alessia.esposito11@hotmail.com>
2023-03-09 15:24:39 -0500

    Commencer intégration du menu

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,4 +1,28 @@
 <x-layout>
-<h1>Accueil</h1>
+
+    <nav class="navbar menu bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="/storage/logo.png" alt="Logo" width="100">
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Activités</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Forfaits</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="#">Connexion</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
 </x-layout>
