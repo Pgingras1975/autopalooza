6f59bcd5753e3c235371faa9c05a572d57eb2078
ALESSIA\aless <alessia.esposito11@hotmail.com>
2023-03-10 11:57:36 -0500

    Créer le menu et le header de la page accueil. Rajouter le lien pour les polices dans les layouts.

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,9 +1,9 @@
-<x-layout>
+<x-layout-accueil>
 
     <nav class="navbar menu bg-body-tertiary">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="/storage/logo.png" alt="Logo" width="100">
+                <img src="/storage/logo.png" alt="Logo AutoPalooza" width="100">
             </a>
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -18,11 +18,27 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact</a>
                 </li>
-                <li>
-                    <a class="nav-link" href="#">Connexion</a>
+                <li class="nav-item">
+                    <a class="nav-link connexion" href="#">Connexion</a>
                 </li>
             </ul>
         </div>
     </nav>
+
+    <main>
+        <header>
+            <div class="video-container">
+                <h1>Festival AutoPalooza</h1>
+                <h2>Du 4 au 6 août 2023</h2>
+                <video autoplay loop>
+                    <source src="/storage/video_header.mp4" type="video/mp4">
+                </video>
+            </div>
+
+            <div class="flesh-defilement">
+                <img src="/storage/flesh.png" alt="" width="50">
+            </div>
+        </header>
+    </main>
 
 </x-layout>
