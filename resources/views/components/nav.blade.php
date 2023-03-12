fe1746c29b27bec88e2b2a2e77058bedf6ed5cf4
ALESSIA\aless <alessia.esposito11@hotmail.com>
2023-03-12 16:39:33 -0400

    Ajout component nav pour le menu. Avancer le style dans la page accueil.

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,26 @@
+@props(["nav"])
+
+<nav class="navbar menu bg-body-tertiary sticky-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="/storage/logo.png" alt="Logo AutoPalooza" width="100">
+        </a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Activités</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Forfaits</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link connexion" href="#">Connexion</a>
+            </li>
+        </ul>
+    </div>
+</nav>
