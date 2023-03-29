2d4f59843e6287eaf29e51300620c354ee468eea
CSTJ\9246343 <pgingra1@gmail.com>
2023-03-29 11:51:34 -0400

    refactoring 4 vues

--- resources/views/activite/modifier.blade.php
+++ resources/views/activite/modifier.blade.php
@@ -1,87 +1,78 @@
 <x-dashboard-layout>
 
-    <div id="wrapper">
+    <x-nav-dashboard :authuser="$authuser" :authuserid="$authuserid"/>
 
-        <x-nav-dashboard :authuser="$authuser" :authuserid="$authuserid"/>
+    <div class="wrapper">
 
-        <!--  page-wrapper -->
-        <div id="page-wrapper" class="wrapper">
+        <div class="row">
+            <div class="padding-top-form"></div>
+            <div class="col-lg-3">
 
-            <div class="row">
-                <div class="padding-top-form"></div>
-                <div class="col-lg-3">
+            </div>
 
-                </div>
+            <div class="col-lg-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading entete header-form-h" style="background-color:#e71d36">
+                        <p class="header-form-fs"><i class="fa fa-bar-chart-o fa-fw"></i>Activités</p>
+                        <form method="POST" action="{{ route('activite.delete', $activite->id) }}">
+                            @csrf
+                            <input name="_method" type="hidden" value="DELETE">
+                            <button type="submit" class="btn-supprimer show_confirm" data-toggle="tooltip" title='Delete'>Supprimer</button>
+                        </form>
+                    </div>
 
-                <div class="col-lg-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading entete header-form-h" style="background-color:#e71d36">
-                            <p class="header-form-fs"><i class="fa fa-bar-chart-o fa-fw"></i>Activités</p>
-                            <form method="POST" action="{{ route('activite.delete', $activite->id) }}">
+                    <div class="panel-body form-h">
+                        <div class="container">
+                            <form action="{{ url('/activite/modifier/' . $activite->id ) }}" method="post" enctype="multipart/form-data">
                                 @csrf
-                                <input name="_method" type="hidden" value="DELETE">
-                                <button type="submit" class="btn-supprimer show_confirm" data-toggle="tooltip" title='Delete'>Supprimer</button>
+
+                                @error('texte')
+                                    <p class="alert alert-danger">{{ $message }}</p>
+                                @enderror
+
+                                <div class="row">
+                                    <div class="col-25">
+                                        <label for="nom">Nom</label>
+                                    </div>
+                                    <div class="col-75">
+                                        <input type="text" id="nom" name="nom" value="{{ $activite->nom }}">
+                                        <x-form-message champ="nom" />
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-25">
+                                        <label for="subject">Description</label>
+                                    </div>
+                                    <div class="col-75">
+                                        <textarea id="description" name="description" style="height:200px">{{ $activite->description }}</textarea>
+                                        <x-form-message champ="description" />
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-25">
+                                        <label for="image">Image : </label>
+                                        <img src="{{ $activite->image }}" alt="" width="75">
+                                        </div>
+                                        <div class="col-75">
+                                        <input type="file" name="image" id="">
+                                    </div>
+                                    <x-form-message champ="image" />
+                                </div>
+
+                                <div class="row">
+                                    <div class="btn-position">
+                                        <input class="btn-modifier" onclick="opacityFadeIn()" type="submit" value="Modifier" >
+                                        <a class="btn-annuler" onclick="opacityFadeIn()" href="{{ route('admin') }}">Annuler</a>
+                                    </div>
+                                </div>
                             </form>
                         </div>
-
-                        <div class="panel-body form-h">
-                            <div class="container">
-                                <form action="{{ url('/activite/modifier/' . $activite->id ) }}" method="post" enctype="multipart/form-data">
-                                    @csrf
-
-                                    @error('texte')
-                                        <p class="alert alert-danger">{{ $message }}</p>
-                                    @enderror
-
-                                    <div class="row">
-                                        <div class="col-25">
-                                            <label for="nom">Nom</label>
-                                        </div>
-                                        <div class="col-75">
-                                            <input type="text" id="nom" name="nom" value="{{ $activite->nom }}">
-                                            <x-form-message champ="nom" />
-                                        </div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-25">
-                                            <label for="subject">Description</label>
-                                        </div>
-                                        <div class="col-75">
-                                            <textarea id="description" name="description" style="height:200px">{{ $activite->description }}</textarea>
-                                            <x-form-message champ="description" />
-                                        </div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-25">
-                                            <label for="image">Image : </label>
-                                            <img src="{{ $activite->image }}" alt="" width="75">
-                                         </div>
-                                         <div class="col-75">
-                                            <input type="file" name="image" id="">
-                                        </div>
-                                        <x-form-message champ="image" />
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="btn-position">
-                                            <input class="btn-modifier" onclick="opacityFadeIn()" type="submit" value="Modifier" >
-                                            <a class="btn-annuler" onclick="opacityFadeIn()" href="{{ route('admin') }}">Annuler</a>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <!-- /.panel-body -->
-                    <div>
-                    <!-- /.panel-primary -->
+                    </div>
                 <div>
             <div>
-        </div>
-        <!-- end page-wrapper -->
-
+        <div>
     </div>
-    <!-- end wrapper -->
 
 </x-dashboard-layout>
